14a8b728a05db6e35f3918193e020538597dd970
EdisonChan <148373644@qq.com>
2026-04-28 15:11:01 +0800

    refactor: 优化 H5 测试页按钮功能与 nvue 日志样式

--- App.vue
+++ App.vue
@@ -1,17 +1,37 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch() {
+    const x5Local = uni.requireNativePlugin("X5LocalInstallModule");
+    x5Local.checkX5Installed((res) => {
+      if (!res.installed) {
+        x5Local.installX5Core({}, (r) => {
+          if (r.success && r.needRestart) {
+            uni.showModal({
+              title: "提示",
+              content: "X5内核安装成功，是否立即重启应用？",
+              confirmText: "立即重启",
+              cancelText: "稍后重启",
+              success: (res) => {
+                if (res.confirm) {
+                  x5Local.restartApp(() => {});
+                }
+              },
+            });
+          }
+        });
+      }
+    });
+  },
+
+  onShow: function () {
+    console.log("App Show");
+  },
+  onHide: function () {
+    console.log("App Hide");
+  },
+};
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
 </style>
